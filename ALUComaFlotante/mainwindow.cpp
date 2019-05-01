49b087863d24a938fbac2ec81a0a4c49ca6138be
tlemar00 <36696289+tlemar00@users.noreply.github.com>
2019-05-01 13:10:42 +0200

    Primeros pasos para pasar el nº a coma flotante

--- ALUComaFlotante/mainwindow.cpp
+++ ALUComaFlotante/mainwindow.cpp
@@ -24,10 +24,24 @@ void MainWindow::on_botonSuma_clicked()
     //Leemos el primer numero
     real1 = ui->lineEdit->text();
     num1 =  real1.toFloat();
-
+    //PRIMER PASO: Calculamos el bit de signo
+    if(num1 > 0){ //El numero es positivo
+        signo = 0;
+    }else{ // El numero es negativo
+        signo = 1;
+    }
+    //SEGUNDO PASO: Normalizamos la mantisa. (se haya dividiendo por dos el entero y quedándote con el resto
+    //y multiplicando por 2 la parte fraccionaria y quedándote con los resultados enteros)
+        
     //Leemos el segundo numero
     real2 = ui->lineEdit_2->text();
     num2 = real2.toFloat();
+    //Calculamos el bit de signo
+    if(num2 > 0){
+        signo = 0;
+    }else{
+        signo = 1;
+    }
 }
 
 void MainWindow::on_botonResta_clicked()
@@ -35,10 +49,22 @@ void MainWindow::on_botonResta_clicked()
     //Leemos el primer numero
     real1 = ui->lineEdit->text();
     num1 =  real1.toFloat();
+    //Calculamos el bit de signo
+    if(num1 > 0){ 
+        signo = 0;
+    }else{ 
+        signo = 1;
+    }
 
     //Leemos el segundo numero
     real2 = ui->lineEdit_2->text();
     num2 = real2.toFloat();
+    //Calculamos el bit de signo
+    if(num2 > 0){
+        signo = 0;
+    }else{
+        signo = 1;
+    }
 }
 
 void MainWindow::on_botonProducto_clicked()
@@ -46,10 +72,22 @@ void MainWindow::on_botonProducto_clicked()
     //Leemos el primer numero
     real1 = ui->lineEdit->text();
     num1 =  real1.toFloat();
-
+    //Calculamos el bit de signo
+    if(num1 > 0){ 
+        signo = 0;
+    }else{ 
+        signo = 1;
+    }
+    
     //Leemos el segundo numero
     real2 = ui->lineEdit_2->text();
     num2 = real2.toFloat();
+    //Calculamos el bit de signo
+    if(num2 > 0){ 
+        signo = 0;
+    }else{ 
+        signo = 1;
+    }
 }
 
 void MainWindow::on_botonDivision_clicked()
@@ -57,9 +95,20 @@ void MainWindow::on_botonDivision_clicked()
     //Leemos el primer numero
     real1 = ui->lineEdit->text();
     num1 =  real1.toFloat();
-
+    //Calculamos el bit de signo
+    if(num1 > 0){ 
+        signo = 0;
+    }else{ 
+        signo = 1;
+    }
+    
     //Leemos el segundo numero
     real2 = ui->lineEdit_2->text();
     num2 = real2.toFloat();
-
+    //Calculamos el bit de signo
+    if(num2 > 0){
+        signo = 0;
+    }else{
+        signo = 1;
+    }
 }
